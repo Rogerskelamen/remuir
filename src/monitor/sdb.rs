7c81c88ac35d6373ebe9d93eb74a786990326d21
Rogerskelamen <rogerkelamen@gmail.com>
2024-04-14 13:26:02 +0800

    add cmd_c

--- src/monitor/sdb.rs
+++ src/monitor/sdb.rs
@@ -4,6 +4,13 @@ use crate::{cpu::exec::cpu_exec, crumble, log};
 
 static mut BATCH_MODE: bool = false;
 
+// /* function pointer for cmd call */
+// type CmdFn = fn(String);
+//
+// struct CmdTable {
+//   
+// }
+
 pub fn sdb_init(is_batch: bool) {
   unsafe {
     BATCH_MODE = is_batch;
@@ -23,6 +30,11 @@ fn rl_get(rl: &mut Editor<()>) -> String {
   }
 }
 
+fn cmd_c(args: &str) {
+  println!("{}", args);
+  cpu_exec(usize::MAX);
+}
+
 pub fn sdb_start() {
   log!("Start sdb!");
   unsafe {
@@ -35,11 +47,31 @@ pub fn sdb_start() {
   // cpu_exec(usize::MAX);
   // return;
 
+  /* create shell-prompt */
   let mut rl = Editor::<()>::new();
   loop {
     let l = rl_get(&mut rl);
-    if let None = l.split_whitespace().next() {
-      continue;
+    let cmd: &str;
+    match l.split_whitespace().next() {
+      Some(s) => {
+        cmd = s;
+      }
+      None => {
+        continue;
+      }
+    }
+    let id = l.find(' ').unwrap_or(l.len());
+    println!("{}", id);
+    let args = if id >= l.len() {
+      ""
+    }else {
+      &l[id + 1..]
+    };
+    match cmd {
+      "c" => {
+        cmd_c(args);
+      }
+      _ => { println!("Unknow command '{}'", cmd)}
     }
     println!("{}", l);
   }

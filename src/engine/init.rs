c150c85f3a6dc4aecd2a2e68d3bb4ee1f189e99d
Rogerskelamen <rogerskelamen@gmail.com>
2024-04-15 10:43:31 +0800

    format

--- src/engine/init.rs
+++ src/engine/init.rs
@@ -2,7 +2,7 @@ use std::process;
 
 use crate::monitor::sdb::sdb_start;
 
-use super::control::{EMUSTATE, ExecState};
+use super::control::{ExecState, EMUSTATE};
 
 pub fn engine_start() {
   sdb_start();
@@ -10,12 +10,10 @@ pub fn engine_start() {
 
 pub fn exit_from_engine() {
   unsafe {
-    if matches!(EMUSTATE.state, ExecState::End) &&
-      EMUSTATE.halt_ret == 0
-    {
+    if matches!(EMUSTATE.state, ExecState::End) && EMUSTATE.halt_ret == 0 {
       println!("Exit safely");
       process::exit(0);
-    }else {
+    } else {
       println!("Exit with specified code [{}]", EMUSTATE.halt_ret);
       process::exit(1);
     }

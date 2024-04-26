c3408cf8562ef8fc802c2b93dd78f32d38ec8e08
Rogerskelamen <rogerkelamen@gmail.com>
2024-04-26 11:06:03 +0800

    add some difftest dut code

--- src/cpu/difftest/dut.rs
+++ src/cpu/difftest/dut.rs
@@ -1,16 +1,34 @@
-use std::path::PathBuf;
+use std::{ffi::c_void, path::PathBuf, sync::Arc};
 
-static mut HAS_DIFFTEST: bool = false;
+struct HasDifftest {
+  exist: bool,
+  libstr: &'static str
+}
+
+static mut HAS_DIFFTEST: HasDifftest = HasDifftest {
+  exist: false,
+  libstr: ""
+};
+
+struct DifftestLib {
+  exist: bool,
+  lib: Arc<libloading::Library>,
+  difftest_memcpy: libloading::Symbol<'static, unsafe extern fn(u32, c_void, isize, bool)>
+}
 
 pub fn init_difftest(diff: Option<PathBuf>) {
   match diff {
-    Some(f) => {
+    Some(path) => {
       unsafe {
-        HAS_DIFFTEST = true;
-        let lib = libloading::Library::new(f).unwrap();
+        HAS_DIFFTEST.exist = true;
+        let path_str = path.as_path().to_str().expect("Failed to load difftest lib path");
+        let boxed_str = path_str.to_owned().into_boxed_str();
+        HAS_DIFFTEST.libstr = Box::leak(boxed_str);
       }
     }
-    None => {
-    }
+    None => {}
   }
 }
+
+// let lib = libloading::Library::new(f).unwrap();
+// let func: libloading::Symbol<unsafe extern fn()> = lib.get(b"difftest_memcpy").unwrap();
